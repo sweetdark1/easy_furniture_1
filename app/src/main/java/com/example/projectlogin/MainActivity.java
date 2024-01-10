0deb1aee2bfb2b68007c366711df074855c239e5
TasnimAyed <tasnimayed4@hotmail.com>
2024-01-10 11:52:52 +0200

    upload project

---
+++ app/src/main/java/com/example/projectlogin/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.projectlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        Button signupButton = findViewById(R.id.signupButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), loginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), signupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
