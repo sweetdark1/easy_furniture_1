4ad40da0a41471193e9dbadc7d1addb05749b272
TasnimAyed <tasnimayed4@hotmail.com>
2024-01-11 14:01:49 +0200

    thursday

--- app/src/main/java/com/example/projectlogin/loginActivity.java
+++ app/src/main/java/com/example/projectlogin/loginActivity.java
@@ -1,8 +1,9 @@
 package com.example.projectlogin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class loginActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class loginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    // Method to handle Forget Password click
+    public void onForgetPasswordClick(View view) {
+        Intent intent = new Intent(this, forgetPasswordActivity.class);
+        startActivity(intent);
     }
 }
